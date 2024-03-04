e9e1e4187c8f843290c112a731cca8cce38185da
Álvaro Quero Ariza <114382675+QueroXD@users.noreply.github.com>
2024-03-04 09:20:22 +0100

    Update

--- php/index.php
+++ php/index.php
@@ -39,44 +39,27 @@ $db = null;
 <head>
 	<title>QueroHacks</title>
   <link rel="stylesheet" href="../css/common.css">
-	<link rel="stylesheet" href="../css/register.css">
+	<!-- <link rel="stylesheet" href="../css/register.css"> -->
   <link rel="icon" type="image/x-icon" href="../img/QueroHacks_Icon.png">
 </head>
 <body>
 <section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
-            <div class="mb-md-5 mt-md-4 pb-5">
-              <h2 class="fw-bold mb-2 text-uppercase">QueroHacks</h2>
-              <p class="text-white-50 mb-5">Entra el teu usuari i contrasenya per entrar!!</p>
-              <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <div class="form-outline form-white mb-4">
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" class="form-control form-control-lg">
-                </div>
-                <br>
-                <br>
-                <div class="form-outline form-white mb-4">
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" class="form-control form-control-lg">
-                </div>
-                <br>
-                <br>
-                <input class="btn btn-outline-light btn-lg px-5" type="submit" value="Submit">
-                <br>
-                <br>
-            </form>
-            </div>
-            <div>
-              <p class="mb-0">No tens un comte? <a href="register.php" class="text-white-50 fw-bold">Sign Up</a>
-              </p>
-            </div>
-          </div>
-        </div>
+  <div id="container-form">
+    <img src="../img/QueroHacks_Logo.jpg" alt="Logo ClassWave" id="iconoEmpresa">
+    <h1>Sign In</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+      <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" class="form-control form-control-lg">
       </div>
+      <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" class="form-control form-control-lg">
+      </div>
+      <input type="submit" value="SUBMIT" id="button">
+    </form>
+    <div>
+      <footer class="mb-0">New user? <a href="register.php" class="text-white-50 fw-bold">Sign Up</a></footer>
     </div>
   </div>
 </section>
